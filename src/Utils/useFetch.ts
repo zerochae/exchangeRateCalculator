29199e9358450493f6ca8e88c2f07bbf78f3007a
zerochae <zerochae@kakao.com>
2022-03-15 17:30:43 +0900

    Feat: 커스텀 훅 추가

--- src/Utils/useFetch.ts
+++ src/Utils/useFetch.ts
@@ -1,3 +1,22 @@
-const useFetch = () => {};
+import { useEffect, useState } from "react";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState<any | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        if (data) setData(data);
+      } catch (err) {
+        console.log(err);
+      }
+      fetchData();
+    };
+  }, [setData, url]);
+
+  return [...data];
+};
 
 export default useFetch;
