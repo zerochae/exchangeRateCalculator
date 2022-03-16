7205d1fc8a710d4f59e1ba9e903835060d24b3fe
zerochae <zerochae@kakao.com>
2022-03-16 20:48:53 +0900

    Refactor: 코드 리팩토링

--- src/Components/Alert/index.Alert.tsx
+++ src/Components/Alert/index.Alert.tsx
@@ -7,16 +7,16 @@ import { useSelector } from "react-redux";
 import useCalculate from "Utils/useCalculate";
 import { useEffect, useState } from "react";
 
-const Alert = () => {
+const Alert = (): JSX.Element => {
   const input = useSelector((state: RootState) => state.input.price);
   const sender = useSelector((state: RootState) => state.select.sender);
   const receiver = useSelector((state: RootState) => state.select.receiver);
   const isClick = useSelector((state: RootState) => state.button.isClick);
   const isFirst = useSelector((state: RootState) => state.button.isFirst);
 
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState<string>("");
 
-  const calResult = useCalculate(
+  const calResult: string = useCalculate(
     parseInt(input),
     C.RECEIVER[receiver].price,
     C.SENDER[sender].price
